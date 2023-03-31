0acd6c6745033f2a652386819508dbbc3c0daf01
unknown <marufmozumder7@gmail.com>
2023-03-31 02:31:26 +0200

    first-challenge-done

--- src/challenge-1.js
+++ src/challenge-1.js
@@ -11,4 +11,55 @@ console.log(studentNames);
 let truthOrFalse = true || false;
 // Challenge 5: Simulate a Type Error by assigning a number to a boolean
 // let truthOrDare: boolean = 34;
-// Challenge 6:
+// Challenge 6:Get the length of an arbitrary string
+const isArray = "TypeScript";
+console.log(isArray.length);
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 1 ------------------
+Assign a number to a variable with no type annotation, what is the variable type inferred by TypeScript?
+*/
+let random = 3;
+console.log(random);
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 1 ------------------
+Create an array of numbers
+*/
+const everybody = [20, 30];
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 2 ------------------
+From the previously create array, access the second-indexed item
+*/
+let everybodyOkay = everybody[1];
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 3 ------------------
+Create an array of the two boolean values
+*/
+const booleanValues = [true, false];
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 1 ------------------
+Create a string type tuple
+*/
+const stringTuple = ["Hello", "World"];
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 3 ------------------
+Create a string and number type tuple and access the second item
+*/
+const mixTuple = ["You", 3];
+mixTuple[1];
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 1 ------------------
+Create an enum of product names and display it on the console
+*/
+var ProductsName;
+(function (ProductsName) {
+    ProductsName[ProductsName["Hello"] = 0] = "Hello";
+    ProductsName[ProductsName["From"] = 1] = "From";
+    ProductsName[ProductsName["the"] = 2] = "the";
+    ProductsName[ProductsName["other"] = 3] = "other";
+    ProductsName[ProductsName["side"] = 4] = "side";
+})(ProductsName || (ProductsName = {}));
+console.log(ProductsName);
+// let productName: ProductsName = ProductsName.Hello;
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 2 ------------------
+Create an enum of product ids and display it on the console
+*/
+var ProductId;
+(function (ProductId) {
+    ProductId[ProductId["id"] = 1] = "id";
+    ProductId["name"] = "Kothin";
+})(ProductId || (ProductId = {}));
+const productId = ProductId.id;
+console.log(productId);
--- src/challenge-1.ts
+++ src/challenge-1.ts
@@ -15,4 +15,64 @@ let truthOrFalse: boolean = true || false;
 // Challenge 5: Simulate a Type Error by assigning a number to a boolean
 // let truthOrDare: boolean = 34;
 
-// Challenge 6:
+// Challenge 6:Get the length of an arbitrary string
+const isArray: string = "TypeScript";
+console.log(isArray.length);
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 1 ------------------
+Assign a number to a variable with no type annotation, what is the variable type inferred by TypeScript?
+*/
+let random = 3;
+console.log(random);
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 1 ------------------
+Create an array of numbers
+*/
+const everybody: number[] = [20, 30];
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 2 ------------------
+From the previously create array, access the second-indexed item
+*/
+let everybodyOkay = everybody[1];
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 3 ------------------
+Create an array of the two boolean values
+*/
+const booleanValues: boolean[] = [true, false];
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 1 ------------------
+Create a string type tuple
+*/
+const stringTuple: [string, string] = ["Hello", "World"];
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 3 ------------------
+Create a string and number type tuple and access the second item
+*/
+const mixTuple: [string, number] = ["You", 3];
+mixTuple[1];
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 1 ------------------
+Create an enum of product names and display it on the console
+*/
+enum ProductsName {
+  Hello,
+  From,
+  the,
+  other,
+  side,
+}
+
+console.log(ProductsName);
+
+// let productName: ProductsName = ProductsName.Hello;
+
+/* *-*-*-*-*-*-*-*-*-*-* Challenge 2 ------------------
+Create an enum of product ids and display it on the console
+*/
+enum ProductId {
+  id = 1,
+  name = "Kothin",
+}
+
+const productId: ProductId = ProductId.id;
+console.log(productId);
